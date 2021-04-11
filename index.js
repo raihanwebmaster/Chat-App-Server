95e757fdd03750f6e07fc4da35515f01f316b561
raihanwebmaster <raihanemon2015@gmail.com>
2021-04-11 21:25:24 +0200

    server

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: "https://hardcore-heisenberg-fd4ac0.netlify.app/",
+    origin: "https://hardcore-heisenberg-fd4ac0.netlify.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
